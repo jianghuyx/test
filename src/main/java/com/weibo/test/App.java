9e33801865e4e0de03e410bf0a35cc1cd95a1b25
jianghuyx <545582898@qq.com>
2016-11-10 14:26:09 +0800

    Update App.java

--- src/main/java/com/weibo/test/App.java
+++ src/main/java/com/weibo/test/App.java
@@ -1,12 +1,12 @@
-//package com.weibo.test;
-//
-///**
-// * Hello world!
-// *
-// */
-//public class App 
-//{
-//    public int add(int a, int b) {
-//    	return a + b;
-//    }
-//}
+package com.weibo.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public int add(int a, int b) {
+    	return a + b;
+    }
+}
